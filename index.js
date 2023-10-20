d63e6396ae9c35ece3345916f2b79840ebeddbea
Zafar12345678 <127655821+Zafar12345678@users.noreply.github.com>
2023-10-20 10:46:44 +0530

    just commit

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
 const itemRoutes = require('./routes/itemRoute');
-
-
 
 const app = express();
 
-//const cors = require('cors');
-//app.use(cors());
+// Enable All CORS Requests
+app.use(cors());
 
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api', itemRoutes);
 
 // Database connection
 mongoose.connect("mongodb+srv://merningday123:merningday123@cluster0.6llqgoh.mongodb.net/software?retryWrites=true&w=majority", {
@@ -21,13 +25,6 @@ mongoose.connect("mongodb+srv://merningday123:merningday123@cluster0.6llqgoh.mon
 .catch((error) => {
   console.error("Error connecting to MongoDB:", error);
 });
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use('/api', itemRoutes);
-
 
 // Start the server
 const PORT = 3000;
